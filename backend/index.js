3922c345e7ae3672a37e071e85cb6440a6523486
Sachin-8125 <devinewolf7@gmail.com>
2026-09-25 10:48:48 +0530

    feat: establish database connection and start server with environment configuration

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import { app } from "./app.js";
+import { connectDB } from "./db/connect.js";
+import { env } from "./config/env.js";
 
 async function start() {
     try {
-
+        await connectDB();
+        app.listen(env.PORT, () => {
+            console.log(`Server is running on port ${env.PORT}`);
+        });
     } catch (error) {
-
+        console.log("Error starting server: ", error);
+        process.exit(1);
     }
 }
 
